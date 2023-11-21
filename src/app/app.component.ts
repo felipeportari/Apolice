443591e56280f36e60898ced4f39a199b1dbef94
felipeportari <felipe.portari7@gmail.com>
2023-11-20 21:26:21 -0300

    Adicionando apolice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ApoliceComponent } from './apolice/apolice.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ApoliceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
